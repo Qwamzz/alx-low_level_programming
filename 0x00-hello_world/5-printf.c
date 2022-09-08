963627e1aac5cb8b61cae03c94cc121e71bb61a5
Qwamzz <niiyarteygidiglo@gmail.com>
2022-09-08 11:51:50 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
 * main - print the string
 *
+* Description: using the main function
+* this program prints "with proper grammar, but the outcome is a piece of art"
 * Return: 0
 */
 int main(void)
